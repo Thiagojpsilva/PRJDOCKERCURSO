f8352c67906b27851163f4fc1aedf04a9472666a
root <root@debian01.us-west1-b.c.tjestudos2019.internal>
2019-01-17 15:34:20 +0000

    Firts Commit Curso Docker

---
+++ email-worker-compose/scripts/init.sql
@@ -0,0 +1,13 @@
+Create database email_sender;
+
+\c email_sender
+
+Create table emails(
+	id serial not null,
+	data timestamp not null default current_timestamp,
+	assunto varchar(100) not null,
+	mensagem varchar(250) not null);
+
+
+
+
